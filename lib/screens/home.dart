19c2fc4f5f736cffe227cf5f1a62449d10589df2
masterONE <dchatchai@gmail.com>
2019-10-09 16:21:25 +0700

    Success UI

---
+++ lib/screens/home.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:onerci/screens/my_style.dart';
+import 'package:onerci/screens/register.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+// Explicit ประกาศตัวแปร
+
+// Method
+
+  Widget signInButton() {
+    return RaisedButton(
+      color: MyStyle().textColor,
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      child: Text('Sign UP'),
+      onPressed: () {
+        print('You Click Sign Up');
+
+        MaterialPageRoute materialPageRoute = MaterialPageRoute(builder: (BuildContext context) => Register());
+        Navigator.of(context).push(materialPageRoute);
+
+
+
+      },
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      children: <Widget>[
+        signInButton(),
+        SizedBox(
+          width: 4.0,
+        ),
+        signUpButton(),
+      ],
+      mainAxisSize: MainAxisSize.min,
+    );
+  }
+
+  Widget passwordText() {
+    return Container(
+      child: TextFormField(
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.lock,
+            size: 36.0,
+            color: MyStyle().textColor,
+          ),
+          labelText: 'Password :',
+          labelStyle: TextStyle(color: MyStyle().textColor),
+        ),
+      ),
+      width: 250.0,
+    );
+  }
+
+  Widget userText() {
+    return Container(
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.account_circle,
+            size: 36.0,
+            color: MyStyle().textColor,
+          ),
+          labelText: 'User :',
+          labelStyle: TextStyle(color: MyStyle().textColor),
+        ),
+      ),
+      width: 250.0,
+    );
+  }
+
+  Widget showLogo() {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      'One RCI',
+      style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic,
+          color: Colors.blueAccent[100],
+          fontFamily: 'BalooBhai'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [Colors.white, MyStyle().mainColor],radius: 1.0,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                showLogo(),
+                showAppName(),
+                userText(),
+                passwordText(),
+                SizedBox(
+                  height: 8.0,
+                ),
+                showButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/my_style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class MyStyle {
+  // Fielld
+  double h1 = 30.0, h2 = 18.0;
+  Color textColor = Colors.blueAccent[100];
+  Color mainColor = Color.fromARGB(255, 223, 77, 96);
+
+  // Contructor
+  MyStyle();
+
+}
